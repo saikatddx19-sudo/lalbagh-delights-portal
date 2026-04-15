5b5cd98e1a30129f9229fc29d0e69787db6fa2da
saikatddx19-sudo <saikatddx19@gmail.com>
2026-04-15 13:24:21 +0600

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 import "../index.css";
-import "./navbar.css";
 
 // Smooth scroll (Lenis)
 import Lenis from "lenis";
